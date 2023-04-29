047616e108426d8d9c5e3eef0e9274c3f4b537bf
Marisa Tania <mtania@dons.usfca.edu>
2023-04-28 19:52:15 -0700

    Use Multi-Line Docstring format for class and function documentations

--- click_counter.py
+++ click_counter.py
@@ -4,6 +4,19 @@ from collections import Counter
 
 
 class ClickCounter:
+    """A class for counting the number of clicks for a list of URLs.
+
+    Args:
+        encoded_data_file (str): The path to the CSV file containing long url and shortened links or "encodes".
+        decoded_data_file (str): The path to the JSON file containing raw data on Bitlink clicks.
+
+    Attributes:
+        encoded_data_file (str): The path to the CSV file containing the encoded URL data.
+        decoded_data_file (str): The path to the JSON file containing the decoded URL data.
+        encoding_map (dict): A dictionary containing the mapping of Bitlink URL to long URL.
+        clicks (dict): A dictionary containing the counts of clicks for each long URL.
+    """
+
     def __init__(self, encoded_data_file, decoded_data_file):
         self.encoded_data_file = encoded_data_file
         self.decoded_data_file = decoded_data_file
@@ -12,6 +25,12 @@ class ClickCounter:
         self.result = {}
 
     def get_encoded_data(self):
+        """Read the CSV file and return the data.
+
+        Returns:
+            list: The data from the encoded data file as a list of lists.
+
+        """
         with open(self.encoded_data_file, 'r') as csvfile:
             reader = csv.reader(csvfile)
             # Skip the first row (header)
@@ -20,18 +39,20 @@ class ClickCounter:
         return raw_data
 
     def create_encoding_map(self):
+        """Create a dictionary mapping the Bitlink URL to long URL from raw data."""
         raw_data = self.get_encoded_data()
         for record in raw_data:
             key = f'http://{record[1]}/{record[2]}'
             value = record[0]
             self.encoding_map.update({key: value})
 
-    def get_bitlink_url(self):
+    def get_decoded_url(self):
         """
-        Parse a JSON file and return the bitlink URL.
+        Parse a JSON file, checks if the bitlink url is in encoding map,
+        and return a list of decoded long URL.
 
         Returns:
-            str: The bitlink URL from the JSON data in the decodes data file.
+            str: The long URL list from the JSON data
 
         Raises:
             FileNotFoundError: If the decodes data file does not exist.
@@ -57,11 +78,20 @@ class ClickCounter:
             raise KeyError("JSON data does not contain a 'bitlink' key")
 
     def count_clicks(self):
-        decoded_url = self.get_bitlink_url()
+        """
+        Count the number of clicks for each long URL
+        and save the url and its click count in clicks map
+        """
+        decoded_url = self.get_decoded_url()
         self.clicks = dict(Counter(decoded_url))
 
     def get_sorted_result(self):
-        return [dict(sorted(self.clicks.items(), key=lambda item: item[1], reverse=True))]
+        """
+        Sort the results of click counts in descending order
+        and return them as a list of dictionary.
+        """
+        self.clicks = dict(sorted(self.clicks.items(), key=lambda item: item[1], reverse=True))
+        return [self.clicks]
 
 
 def main():
